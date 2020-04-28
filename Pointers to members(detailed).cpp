43a08cf82dcdc8bd682c7b2722095a15136b31ff
Gautam Goyal <60264618+Gautam-Goyal@users.noreply.github.com>
2020-04-28 23:22:37 +0530

    Pointers to all types of member of classes

---
+++ Pointers to members(detailed).cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+class M
+{
+	int x;
+	int y;
+public:
+	void setxy(int a,int b)
+	{
+		x=a;
+		y=b;
+	}
+	friend int sum(M m);
+};
+int sum(M m)
+{
+	int M::*px = &M::x;//pointer px to member x of class M which stores x address.
+	int M::*py = &M::y;//pointer py to member y of class M which stores y address.
+	M*pm = &m;         //pointer pm which stores address of object m of class M.
+	int s = m.*px + pm->*py;
+	return s;
+}
+int main()
+{
+	M n;
+	void(M::*pf)(int,int) = &M::setxy;//pointer to member function.
+	(n.*pf)(10,20);                   //n object calling setxy function using pf pointer.
+	cout<<"sum="<<sum(n)<<"\n";
+
+	M*op = &n;                        //pointer to address of n object of class M.
+	(op->*pf)(30,40);
+	cout<<"sum="<<sum(n)<<"\n";
+	return 0;
+}
